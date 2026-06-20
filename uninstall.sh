d3611f5f6406fa26ec5528be7537623b8a403947
Ali Baasiri <ali@baasiri.ir>
2026-06-20 09:45:56 +0330

    uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -4,7 +4,7 @@ echo "🛑 Uninstalling PassWall2 LED Controller..."
 /etc/init.d/passwallled stop 2>/dev/null || true
 /etc/init.d/passwallled disable 2>/dev/null || true
 rm -f /etc/init.d/passwallled
-rm -rf /root/passwall2-led-v6
+rm -rf /root/passwall2-led-ac1304
 rm -f /tmp/passwall_*
 
 echo "✅ حذف کامل انجام شد."
